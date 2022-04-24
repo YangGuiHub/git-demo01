5435d02c300b5ca2a7a8bb25b1857d7cb118879f
June <96237186+YangGuiHub@users.noreply.github.com>
2022-04-24 15:18:35 +0800

    SSH 测试成功

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
   	public static void main(String[] args) {
     		System.out.println("Hello world");
-		system.out.println("Hello github");
+		System.out.println("Hello github");
+		System.out.println("测试 拉取");
   	}
 }
